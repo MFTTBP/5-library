521d55114b3c9b83f2864fe59ca210c2e52e2bf3
MFTTBP <rebro_leorika@bk.ru>
2026-01-19 15:08:29 +0300

    projec

--- library.py
+++ library.py
@@ -1,3 +1,6 @@
+""""
+this is ,y library project
+"""
 library: dict[str, str] = {"Кладбище домашних животных": "Steaven King", "Долгая дорога" : "Steaven King", "Долгие ночи" : "А.А. Айдамиров"}
 
 authors = set(library.values())
