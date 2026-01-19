ec19cbfa5e88e06d21657bb222e924f976f9eeca
MFTTBP <rebro_leorika@bk.ru>
2026-01-19 15:06:46 +0300

    proect

---
+++ library.py
@@ -0,0 +1,6 @@
+library: dict[str, str] = {"Кладбище домашних животных": "Steaven King", "Долгая дорога" : "Steaven King", "Долгие ночи" : "А.А. Айдамиров"}
+
+authors = set(library.values())
+
+print(library.keys())
+print(authors)
